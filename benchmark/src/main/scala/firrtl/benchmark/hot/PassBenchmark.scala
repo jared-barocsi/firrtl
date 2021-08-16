b1ae071953d028952051e5bf5ed149774814653c
Jared Barocsi <jared.barocsi@sifive.com>
2021-08-16 15:45:02 -0700

    Remove double assignment compilation error

--- benchmark/src/main/scala/firrtl/benchmark/hot/PassBenchmark.scala
+++ benchmark/src/main/scala/firrtl/benchmark/hot/PassBenchmark.scala
@@ -13,7 +13,6 @@ abstract class PassBenchmark(passFactory: () => Pass) extends App {
   val warmup = args(1).toInt
   val runs = args(2).toInt
 
-  val input = filenameToCircuit(inputFile)
   val input = directoryToCircuit(inputFile)
   val inputState = CircuitState(input, ChirrtlForm)
 
